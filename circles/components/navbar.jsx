17b2781c4fce3b47b86cd8495afd2f63498b08aa
dannybains2 <dannybains2@gmail.com>
2023-01-11 16:00:42 +0000

    check navbar

--- circles/components/navbar.jsx
+++ circles/components/navbar.jsx
@@ -72,26 +72,3 @@ const handleNav = () => {
     )
 }
 export default Navbar
-
-                // {/* mobile button */}
-                // <div className='block sm:hidden z-10'>
-                //     <AiOutlineMenu size={20}/>
-                // </div>
-
-                // {/* mobile Menu */}
-                // <div className='sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black ease-in duration-300'>
-                // <ul>
-                //     <li className='p-4 text-4xl hover:text-gray-500'>
-                //         <Link href='/'>Home</Link>
-                //     </li>
-                //     <li className='p-4 text-4xl hover:text-gray-500'>
-                //         <Link href='/'>Events</Link>
-                //     </li>
-                //     <li className='p-4 text-4xl hover:text-gray-500'>
-                //         <Link href='/'>Profile</Link>
-                //     </li>
-                //     <li className='p-4 text-4xl hover:text-gray-500'>
-                //         <Link href='/'>FAQs</Link>
-                //     </li>
-                // </ul>
-                // </div>
