689b06fa74d37b1a2e6df1065e392db6824807bd
ionajosephine <ionajosephine@gmail.com>
2023-01-12 17:30:10 +0100

    Add button component, fiddling with YourGroups component

---
+++ circles/components/YourGroups.jsx
@@ -0,0 +1,27 @@
+import Button from "./Button";
+
+export default function YourGroups() {
+  return (
+   
+     
+      <div>
+     
+        <div className="flex flex-1 flex-col p-8">
+        <img
+          className="inline-block h-14 w-14 rounded-full"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+        />
+      <h1>Sadako Takashi</h1>
+      <h3>Pending Requests</h3>
+      <br></br>
+      <Button/><br></br>
+      <Button/><br></br>
+      <Button/><br></br>
+      <Button/>
+
+        </div>
+        </div>
+
+  )
+}
--- circles/pages/profile.jsx
+++ circles/pages/profile.jsx
@@ -1,13 +1,11 @@
 import GroupsGallery from "../components/GroupsGallery"
 import Calendar from "../components/Calendar"
+import YourGroups from "../components/YourGroups"
 
 export default function Profile() {
   return (
-    <div>
-      <Calendar/>
-      <h1>Groups you are a member of</h1>
-
-      <GroupsGallery />
+    <div className="fixed left-0 top-0 w-full z-50 relative">
+      <YourGroups/>
     </div>
   )
 }
