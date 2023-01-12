3fee0b051ce2a21ac830a8c5a5d5b8a34600ced0
ionajosephine <ionajosephine@gmail.com>
2023-01-12 18:58:54 +0100

    YourGroups component has animated buttons

--- circles/components/YourGroups.jsx
+++ circles/components/YourGroups.jsx
@@ -1,26 +1,29 @@
-import Button from "./Button";
 
 export default function YourGroups() {
   return (
    
-     
-      <div>
-     
         <div className="flex flex-1 flex-col p-8">
         <img
-          className="inline-block h-14 w-14 rounded-full"
+          className="inline-block h-[120px] w-[120px] rounded-full"
           src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
           alt=""
         />
       <h1>Sadako Takashi</h1>
-      <h3>Pending Requests</h3>
+      <h3>Your Circles</h3>
       <br></br>
-      <Button/><br></br>
-      <Button/><br></br>
-      <Button/><br></br>
-      <Button/>
-
-        </div>
+      <div>
+      <button className="w-64 inline-flex items-center drop-shadow-xl rounded-md border border-transparent transition ease-in-out delay-150 bg-[#FFD862] hover:-translate-y-1 hover:scale-110 hover:bg-[#80BEAF] duration-300 bg-[#FFD862] px-4 py-2 text-base font-light text-black shadow-sm hover:bg-[#80BEAF] focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+    Book Club </button></div><br></br>
+    <div>
+    <button className="w-64 inline-flex items-center drop-shadow-xl rounded-md border border-transparent transition ease-in-out delay-150 bg-[#FFD862] hover:-translate-y-1 hover:scale-110 hover:bg-[#80BEAF] duration-300 bg-[#FFD862] px-4 py-2 text-base font-light text-black shadow-sm hover:bg-[#80BEAF] focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+    Mindfulness yoga </button></div><br></br>
+    <div>
+    <button className="w-64 inline-flex items-center drop-shadow-xl rounded-md border border-transparent transition ease-in-out delay-150 bg-[#FFD862] hover:-translate-y-1 hover:scale-110 hover:bg-[#80BEAF] duration-300 bg-[#FFD862] px-4 py-2 text-base font-light text-black shadow-sm hover:bg-[#80BEAF] focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+    Singing for the soul </button></div><br></br>
+    <div>
+    <button className="w-64 inline-flex items-center drop-shadow-xl rounded-md border border-transparent transition ease-in-out delay-150 bg-[#FFD862] hover:-translate-y-1 hover:scale-110 hover:bg-[#80BEAF] duration-300 bg-[#FFD862] px-4 py-2 text-base font-light text-black shadow-sm hover:bg-[#80BEAF] focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+    Gardening club </button></div><br></br>
+    
         </div>
 
   )
