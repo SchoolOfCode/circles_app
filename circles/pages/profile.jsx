f63b0c4704a3c5c8ded16a761f7e0394d5d273ca
ionajosephine <ionajosephine@gmail.com>
2023-01-12 15:17:53 +0100

    Render calendar and groups gallery on profile page

--- circles/pages/profile.jsx
+++ circles/pages/profile.jsx
@@ -1,5 +1,13 @@
+import GroupsGallery from "../components/GroupsGallery"
+import Calendar from "../components/Calendar"
+
 export default function Profile() {
   return (
-    <div></div>
+    <div>
+      <Calendar/>
+      <h1>Groups you are a member of</h1>
+
+      <GroupsGallery />
+    </div>
   )
 }
