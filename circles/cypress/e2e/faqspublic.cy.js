9329de34fe70da300bf1b200f68ffceb8336d948
ionajosephine <ionajosephine@gmail.com>
2023-01-16 16:17:50 +0100

    Test passes: when not logged in you can still be able to click on Help and see FAQs

--- circles/cypress/e2e/faqspublic.cy.js
+++ circles/cypress/e2e/faqspublic.cy.js
@@ -1,12 +1,7 @@
-describe('When not logged in yet...', () => {
+describe('When not logged in ...', () => {
   it('should still be able to click on Help and see FAQs', () => {
-    cy.visit('https://circlesapp.netlify.app/')
+    cy.visit('http://localhost:3000')
     cy.get('[data-cy="help"]').click()
-    
-
-    cy.get('.hidden > :nth-child(3) > a').click();
-    cy.get('.bg-gradient-to-b').click();
-    cy.get(':nth-child(1) > .mt-1').click();
     cy.get('[data-cy="faq1heading"]').should('have.text', 'Lorem ipsum dolor sit amet.');
   })
 })
