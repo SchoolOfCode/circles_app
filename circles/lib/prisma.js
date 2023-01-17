66d832fe0d8f4fe5d2430dbe96763156ce359a68
Admin <Admin@Matthews-MacBook-Air.local>
2023-01-17 11:30:40 +0000

    Relation links added to models and pushed to the Postgres Db

--- circles/lib/prisma.js
+++ circles/lib/prisma.js
@@ -1,63 +1,64 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
 } else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
 export default prisma;
 
-/* 
-model groups {
-  id Int @id @default(autoincrement())
-  club_name String
-  days String
-  times String
-  image String
-  tagline String
-  description String
-  location String
-  availability String
-  email String
-  telephone Int
-}
+// model Groups {
+//   id Int @id @default(autoincrement())
+//   club_name String @unique
+//   days String
+//   times String
+//   image String
+//   tagline String
+//   description String
+//   location String
+//   availability String
+//   email String
+//   telephone Int
+// }
 
-model events {
-  id Int @id @default(autoincrement())
-  event_name String
-  date String      //// check format
-  time String      //// is there time format 
-  club_name String    //// coming from groups table
-  running boolean
-}
+// model Events {
+//   id Int @id @default(autoincrement())
+//   event_name String
+//   date String
+//   time String
+//   groups Groups @relation(fields: [club_name], references: [club_name])
+//   club_name String
+//   running Boolean @default(true)
+// }
 
-model users {
-  id Int @id @default(autoincrement())
-  first_name String
-  surname String
-  email String
-  telephone Int
-  role String
-  link_worker_id Int //// coming from same table
-}
+// model Users {
+//   id Int @id @default(autoincrement())
+//   first_name String
+//   surname String
+//   email String
+//   telephone Int
+//   role String
+//   link_worker_id Int
+// }
 
-model feedback {
-  id Int @id @default(autoincrement())
-  user_id Int //// coming from user table(id)
-  event_name String
-  comment String
-  rating Int
-}
+// model Feedback {
+//   id Int @id @default(autoincrement())
+//   user Users @relation(fields: [user_id], references: [id])
+//   user_id Int
+//   event_name String
+//   comment String
+//   rating Int
+// }
 
-model user_circles {
-  id Int @id @default(autoincrement())
-  user_id Int   @relation()
-  club_name String
-}
-*/
+// model User_circles {
+//   id Int @id @default(autoincrement())
+//   user Users @relation(fields: [user_id], references: [id])
+//   user_id Int
+//   club_name String
+// }
