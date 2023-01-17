d72e7798b57593fa5d9b850cd6841c9637e265d7
dannybains2 <dannybains2@gmail.com>
2023-01-17 10:58:24 +0000

    database schema set up

--- circles/lib/prisma.js
+++ circles/lib/prisma.js
@@ -12,3 +12,52 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 export default prisma;
+
+/* 
+model groups {
+  id Int @id @default(autoincrement())
+  club_name String
+  days String
+  times String
+  image String
+  tagline String
+  description String
+  location String
+  availability String
+  email String
+  telephone Int
+}
+
+model events {
+  id Int @id @default(autoincrement())
+  event_name String
+  date String      //// check format
+  time String      //// is there time format 
+  club_name String    //// coming from groups table
+  running boolean
+}
+
+model users {
+  id Int @id @default(autoincrement())
+  first_name String
+  surname String
+  email String
+  telephone Int
+  role String
+  link_worker_id Int //// coming from same table
+}
+
+model feedback {
+  id Int @id @default(autoincrement())
+  user_id Int //// coming from user table(id)
+  event_name String
+  comment String
+  rating Int
+}
+
+model user_circles {
+  id Int @id @default(autoincrement())
+  user_id Int   @relation()
+  club_name String
+}
+*/
