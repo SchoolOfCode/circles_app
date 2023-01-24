c65e0a4b4ecfcc04a9da370b8840e5cc0f83e7e1
camillabrizzi <camillabrizzi@icloud.com>
2023-01-24 15:56:56 +0000

    commented out admin page as it was causing issues with account page, not sure why

--- circles/pages/admin.jsx
+++ circles/pages/admin.jsx
@@ -1,163 +1,142 @@
-import { useSession, getSession } from "next-auth/react";
-import React from "react";
+// import { useSession, getSession } from "next-auth/react";
+// import React from "react";
 
-import Table from "../components/table/table";
-// const data = { user: 'example' };
+// import Table from "./api/admin";
+// // const data = { user: 'example' };
 
+// // fetch("/api/admin", {
+// //   //// Wrap in useEffect
+// //   method: "POST",
 
-// fetch("/api/admin", {
-//   //// Wrap in useEffect
-//   method: "POST",
+// //   headers: {
+// //     "Content-Type": "application/json",
+// //   },
+// //   body: JSON.stringify(data),
+// // })
+// //   .then((response) => response.json())
+// //   .then((data) => {
+// //     console.log("Success:", data);
+// //   })
+// //   .catch((error) => {
+// //     console.error("Error:", error);
+// //   });
 
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify(data),
-// })
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log("Success:", data);
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error);
-//   });
+// // fetch()
 
-// fetch()
+// export default function Admin() {
+//   const session = useSession();
+//   console.log("session", session);
 
+//   const getData = () => [
+//     {
+//       name: "Jane Cooper",
+//       email: "jane.cooper@example.com",
+//       title: "Regional Paradigm Technician",
+//       department: "Optimization",
+//       status: "Active",
+//       role: "Admin",
+//       imgUrl:
+//         "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+//     },
+//     {
+//       name: "Cody Fisher",
+//       email: "cody.fisher@example.com",
+//       title: "Product Directives Officer",
+//       department: "Intranet",
+//       status: "Active",
+//       role: "Owner",
+//       imgUrl:
+//         "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+//     },
+//     {
+//       name: "Esther Howard",
+//       email: "esther.howard@example.com",
+//       title: "Forward Response Developer",
+//       department: "Directives",
+//       status: "Active",
+//       role: "Member",
+//       imgUrl:
+//         "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+//     },
+//     {
+//       name: "Jenny Wilson",
+//       email: "jenny.wilson@example.com",
+//       title: "Central Security Manager",
+//       department: "Program",
+//       status: "Active",
+//       role: "Member",
+//       imgUrl:
+//         "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+//     },
+//     {
+//       name: "Kristin Watson",
+//       email: "kristin.watson@example.com",
+//       title: "Lean Implementation Liaison",
+//       department: "Mobility",
+//       status: "Active",
+//       role: "Admin",
+//       imgUrl:
+//         "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+//     },
+//     {
+//       name: "Cameron Williamson",
+//       email: "cameron.williamson@example.com",
+//       title: "Internal Applications Engineer",
+//       department: "Security",
+//       status: "Active",
+//       role: "Member",
+//       imgUrl:
+//         "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+//     },
+//   ];
 
-export async function getServerSideProps() {
-  let response = await fetch("http://localhost:3000/api/admin");
-  let newData = await response.json();
-  console.log(newData)
-  return { props: { newData } };
-}
+//     const columns = React.useMemo(
+//       () => [
+//         {
+//           Header: "Name",
+//           accessor: "name",
+//         },
+//         {
+//           Header: "Title",
+//           accessor: "title",
+//         },
+//         {
+//           Header: "Status",
+//           accessor: "status",
+//         },
+//         {
+//           Header: "Role",
+//           accessor: "department",
+//         },
+//       ],
+//       []
+//     );
 
-console.log()
-export default function Admin({newData}) {
-  const session = useSession();
-  console.log("session", session);
-  console.log(newData)
+//     const data = React.useMemo(() => getData(), []);
 
-  const getData = () => newData
-  //   {
-  //     name: "Jane Cooper",
-  //     email: "jane.cooper@example.com",
-  //     title: "Regional Paradigm Technician",
-  //     department: "Optimization",
-  //     status: "Active",
-  //     role: "Admin",
-  //     imgUrl:
-  //       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  //   },
-  //   {
-  //     name: "Cody Fisher",
-  //     email: "cody.fisher@example.com",
-  //     title: "Product Directives Officer",
-  //     department: "Intranet",
-  //     status: "Active",
-  //     role: "Owner",
-  //     imgUrl:
-  //       "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  //   },
-  //   {
-  //     name: "Esther Howard",
-  //     email: "esther.howard@example.com",
-  //     title: "Forward Response Developer",
-  //     department: "Directives",
-  //     status: "Active",
-  //     role: "Member",
-  //     imgUrl:
-  //       "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  //   },
-  //   {
-  //     name: "Jenny Wilson",
-  //     email: "jenny.wilson@example.com",
-  //     title: "Central Security Manager",
-  //     department: "Program",
-  //     status: "Active",
-  //     role: "Member",
-  //     imgUrl:
-  //       "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  //   },
-  //   {
-  //     name: "Kristin Watson",
-  //     email: "kristin.watson@example.com",
-  //     title: "Lean Implementation Liaison",
-  //     department: "Mobility",
-  //     status: "Active",
-  //     role: "Admin",
-  //     imgUrl:
-  //       "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  //   },
-  //   {
-  //     name: "danny bains",
-  //     email: "cameron.williamson@example.com",
-  //     title: "Internal Applications Engineer",
-  //     department: "Security",
-  //     status: "Active",
-  //     role: "Member",
-  //     imgUrl:
-  //       "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  //   },
-  // ];
-  
+//   //Optional chaining is required as the session object data is undefined when the page first renders.
+//   if (session.data?.role === "admin") {
+//     return (
+//       <div>
+//       <div>
+//         <h1>Admin Access Only!</h1>
+//         <p>Welcome to the Admin Dashboard</p>
+//       </div>
+//       <div>
+//       <Table columns={columns} data={data} />
+//     </div>
+//     </div>
+//     );
+//   } else {
+//     return (
+//       <div>
+//         <h1> You are not authorized to view this page!</h1>
+//         <h3>
+//           If you require access to this page, please contact
+//           enquiries@circles.com
+//         </h3>
 
-    const columns = React.useMemo(
-      () => [
-        {
-          Header: "id",
-          accessor: "id",
-        },
-        {
-          Header: "First_name",
-          accessor: "first_name",
-        },
-        {
-          Header: "Surname",
-          accessor: "surname",
-        },
-        {
-          Header: "Email",
-          accessor: "email",
-        },
-      ],
-      []
-    );
-  
-    const data = React.useMemo(() => getData(), []);
-
-    
-  
-    
-
-
-
-
-
-
-  //Optional chaining is required as the session object data is undefined when the page first renders.
-  if (session.data?.role === "admin") {
-    return (
-      <div>
-      <div className="ml-5">
-        <h1>Admin Access Only!</h1>
-        <p className="text-2xl">Welcome to the Admin Dashboard</p>
-      </div>
-      <div className="ml-5">
-      <Table columns={columns} data={data} />
-    </div>
-    </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1> You are not authorized to view this page!</h1>
-        <h3>
-          If you require access to this page, please contact
-          enquiries@circles.com
-        </h3>
-        
-      </div>
-    );
-  }
-}
+//       </div>
+//     );
+//   }
+// }
