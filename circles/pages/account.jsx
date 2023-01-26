3672fc4096c3a85530cda27e1f0564fafd084534
Cami <99282527+sea-breezy@users.noreply.github.com>
2023-01-26 11:30:26 +0000

    Update account.jsx

--- circles/pages/account.jsx
+++ circles/pages/account.jsx
@@ -6,7 +6,7 @@ import AccountView from "../components/AccountView";
 export async function getServerSideProps() {
   //let responseEvents = await fetch("http://localhost:3000/api/events");
   let responseEvents = await fetch(
-    `${process.env.CIRCLES_EVENTS_API_ENDPOINT}`
+    "https://circlesapp.netlify.app/api/events"
   );
   let events = await responseEvents.json();
   return { props: { events } };
