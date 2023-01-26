58e5ecb54fbae95a38fe0c6b145bbdf0d7d8e377
camillabrizzi <camillabrizzi@icloud.com>
2023-01-26 10:14:26 +0000

    reverting changes

--- circles/pages/account.jsx
+++ circles/pages/account.jsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 import AccountView from "../components/AccountView";
 
 export async function getServerSideProps() {
-  let responseEvents = await fetch("https://circlesapp.netlify.app/api/events");
-  // let responseEvents = await fetch(
-  //   `${process.env.CIRCLES_EVENTS_API_ENDPOINT}`
-  // );
+  //let responseEvents = await fetch("http://localhost:3000/api/events");
+  let responseEvents = await fetch(
+    `${process.env.CIRCLES_EVENTS_API_ENDPOINT}`
+  );
   let events = await responseEvents.json();
   return { props: { events } };
 }
@@ -19,16 +19,13 @@ export default function Profile({ events }) {
 
   useEffect(() => {
     async function fetchData() {
-      let responseUser = await fetch(
-        "https://circlesapp.netlify.app/api/users",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email: session.data?.user.email }),
-        }
-      );
+      let responseUser = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: session.data?.user.email }),
+      });
 
       let userData = await responseUser.json();
       console.log(userData);
